16273aa03bb9938d1ccbc98cb2704d798b9e19c2
david <david@davidberry.io>
2018-10-10 10:36:21 -0700

    route setup

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 class AboutPage extends React.Component {
   render() {
-    return (`
+    return (
       <div>
         <h1>About</h1>
         <p>RThis application uses React, Redux, React Router and a variety of other helpful libraries</p>
       </div>
-    `);
+    );
   }
 }
 
